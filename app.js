0098c69d6a9d8ca573475f57f5952dd96c43943f
udaydeep28 <udaydeepsinghd@gmail.com>
2025-04-26 23:36:08 +0530

    Update app.listen to use dynamic port

--- app.js
+++ app.js
@@ -114,6 +114,8 @@ app.use((err, req, res, next) => {
   res.status(statusCode);
 });
 
-app.listen(3000, () => {
-  console.log("listening to port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
